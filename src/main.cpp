1f6aa09af9497ef80fb2e0892818c1cedf26e8d6
aaa <abdal.alkilani@gmail.com>
2023-03-08 16:17:06 +0000

    multiple notes

--- src/main.cpp
+++ src/main.cpp
@@ -49,7 +49,7 @@ SemaphoreHandle_t keyArrayMutex;
 SemaphoreHandle_t CAN_TX_Semaphore;
 Knob* volumeKnob;
 //Step sizes
-volatile uint32_t currentStepSize;
+volatile uint32_t currentStepSize[12];
 std::string keyPressed;
 uint8_t TX_Message[8];
 struct Note {
@@ -163,10 +163,32 @@ void generateSineWave(uint32_t phaseAcc, double duration, double samplingRate = 
     }
 }
 
+volatile uint8_t anyKeyPressed = 0;
+int8_t counter = 0;
+int8_t counter2 = 0;
 void sampleISR() {
     static uint32_t phaseAcc = 0;
-    phaseAcc += currentStepSize;
-    // generateSineWave(phaseAcc, duration);
+    counter2++;
+    if(counter2 >= 25) counter2 = 0;
+    if (counter2 == 0){
+        counter++;
+        if(counter >= 12) counter = 0;
+    }
+    
+    if(anyKeyPressed){
+        for(int i = 0; i < 12; i++){
+            if(currentStepSize[counter] !=0){
+                phaseAcc += currentStepSize[counter];
+                delayMicroseconds(10);
+                break;
+            }
+            else{
+                counter++;
+                if(counter >= 12) counter = 0;
+            }
+        }
+    }
+    
     int32_t Vout = (phaseAcc >> 24) - 128;
     Vout = Vout >> (8 - volumeKnob->getCounter());
     analogWrite(OUTR_PIN, Vout + 128);
@@ -207,6 +229,7 @@ uint8_t readCols(){
 	return result;
 }
 
+
 volatile uint8_t keyArray[7];
 void scanKeysTask(void * pvParameters) {
     const TickType_t xFrequency = 50/portTICK_PERIOD_MS;
@@ -214,23 +237,31 @@ void scanKeysTask(void * pvParameters) {
     uint8_t prevKeyArray[3] = {0xF, 0xF, 0xF};
     while(1){
         vTaskDelayUntil( &xLastWakeTime, xFrequency );
-        uint32_t stepsize_local;
+        uint32_t stepsize_local[12];
+        std::fill(stepsize_local, stepsize_local + 12, 0);
+        
+        
         std::string keyPressed_local = "";
         xSemaphoreTake(keyArrayMutex, portMAX_DELAY);
         for(int i = 0; i < 3; i++){
             for(int j = 0; j < 4; j++){
                 if(!(((keyArray[i]) >> j) & 1)){
-                    // std::cout<< ((keyArray[i]) >> j);
+                    anyKeyPressed = 1;
                     keyPressed_local += stepSizes[j + 4*i].name; 
-                    stepsize_local = stepSizes[j+4*i].stepSize;
+                    stepsize_local[j+4*i] = stepSizes[j+4*i].stepSize;
+                }
+                else{
+                    stepsize_local[j+4*i] = 0;
                 }
             }
         }
 
+
         // currentStepSize = (pow(2.0, 32) * 440 / 22000);
         if(keyArray[0] == 0xF && keyArray[1] == 0xF && keyArray[2] == 0xF){
+            anyKeyPressed = 0;
             keyPressed_local = "";
-            stepsize_local = 0;
+            std::fill(stepsize_local, stepsize_local + 12, 0);
         }
         generateMsg(&(keyArray[0]), &prevKeyArray[0]);
         volumeKnob->advanceState((keyArray[3] & 0b0001) | (keyArray[3] & 0b0010));
@@ -238,8 +269,18 @@ void scanKeysTask(void * pvParameters) {
         prevKeyArray[1] = keyArray[1];
         prevKeyArray[2] = keyArray[2];
         xSemaphoreGive(keyArrayMutex);
+        
+        uint32_t time_micro = micros();
+        for(int i = 0; i < 12; i++){
+            
+            __atomic_store_n(&currentStepSize[i], stepsize_local[i], __ATOMIC_RELAXED);
+            // uint32_t a = currentStepSize[i];
+            // Serial.println(a);
+        }
+        
 
-        __atomic_store_n(&currentStepSize, stepsize_local, __ATOMIC_RELAXED);
+        // __atomic_store_n(&currentStepSize, stepsize_local, __ATOMIC_RELAXED);
+        keyPressed = keyPressed_local;
     }
 }
 
